38a5a2b14d1cc4a3293bce16e431157dab966d8c
Martin Otyeka <otyeka.martin@gmail.com>
2022-06-05 22:09:42 -0400

    Initial Commit

---
+++ The Bounce/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  The Bounce
+//
+//  Created by Martin Otyeka on 2022-06-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ The Bounce/The_BounceApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_BounceApp.swift
+//  The Bounce
+//
+//  Created by Martin Otyeka on 2022-06-05.
+//
+
+import SwiftUI
+
+@main
+struct The_BounceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
